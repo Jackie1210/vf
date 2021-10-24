ec3eb27fe9187dcb3c12e388a7c9937ab61149ee
cl <jcignorance@outlook.com>
2021-10-24 20:31:15 +0800

    chore: add demo

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,16 +1,34 @@
 <script setup lang="ts">
-import { main } from 'vf'
+  import { onMounted } from 'vue'
+  import { useVF } from 'vf'
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
-main()
+const sleep = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(1)
+    }, 2000)
+  })
+}
+
+const { isLoading, excute } = useVF(async () => {
+  const res = await sleep()
+  console.log(res)
+}, {
+  catchErr: false
+})
+onMounted(() => {
+  excute()
+})
 </script>
 
 <template>
+  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
